cd723ffb8fb1745822b0926bda7a9be9c8fd2c53
tznind <tznind@dundee.ac.uk>
2025-07-13 09:20:38 +0100

    Fix missing includes and package version

--- templates/basic/Program.cs
+++ templates/basic/Program.cs
@@ -1,5 +1,10 @@
 ﻿using MyGuiCsProject;
 using Terminal.Gui;
+using Terminal.Gui.App;
+using Terminal.Gui.Drawing;
+using Terminal.Gui.Input;
+using Terminal.Gui.ViewBase;
+using Terminal.Gui.Views;
 
 Application.Init();
 
